0ae46a38d064817d76ad9a960fd2c82d1620236d
cleverdog <motoki@cleverdog.me>
2023-04-01 18:37:57 +0900

    メニューの階層化およびショートカットメニューの追加

---
+++ BlazorAppDrawerLayoutSample/wwwroot/clickOutsideHandler.js
@@ -0,0 +1,9 @@
+﻿window.clickOutsideHandler = {
+    addEventListener: function (element, dotNetObject) {
+        window.addEventListener('mousedown', (event) => {
+            if (!element.contains(event.target)) {
+                dotNetObject.invokeMethodAsync('OnClickOutside');
+            }
+        });
+    }
+};
